a6a233b387b052ba84e303881594193c48f1400a
k-jaehyun <wogus146@gmail.com>
2023-12-05 14:36:01 +0900

    init: Create project

---
+++ src/main/java/com/sparta/iforest/IForestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.iforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IForestApplication.class, args);
+	}
+
+}
