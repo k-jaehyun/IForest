a67bc2d46b4f122c270942a3b8f0681341d5e683
k-jaehyun <wogus146@gmail.com>
2023-12-07 19:29:12 +0900

    Fix: WebSecurityConfig logout 경로 허용

--- src/main/java/com/sparta/iforest/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/iforest/config/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/v1/users/signup").permitAll()
                         .requestMatchers("/v1/users/login").permitAll()
+                        .requestMatchers("/v1/users/logout").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
