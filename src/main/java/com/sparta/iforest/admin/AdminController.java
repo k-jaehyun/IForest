0b3c40df33d4cb44fb62a89ea305252515dfab76
yeonwooPark <yeonwoo5115@gmail.com>
2023-12-08 17:02:25 +0900

    feat: 관리자 게시글 수정

--- src/main/java/com/sparta/iforest/admin/AdminController.java
+++ src/main/java/com/sparta/iforest/admin/AdminController.java
@@ -24,18 +24,18 @@ public class AdminController {
     private final AdminService adminService;
 
 
-//    //게시글 수정
-//    @PutMapping("/{postId}")
-//    public ResponseEntity<CommonResponseDto> updatePost(@PathVariable Long postId,
-//                                                        @RequestBody PostRequestDto postRequestDto,
-//                                                        @AuthenticationPrincipal UserDetailsImpl userDetails){
-//        try {
-//            PostResponseDto responseDto = adminService.updatePost(postId,postRequestDto,userDetails.getUsername());
-//            return ResponseEntity.ok().body(responseDto);
-//        }catch (IllegalArgumentException | RejectedExecutionException e){
-//            return ResponseEntity.badRequest().body(new CommonResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
-//        }
-//    }
+    //게시글 수정
+    @PutMapping("/{postId}")
+    public ResponseEntity<CommonResponseDto> updatePost(@PathVariable Long postId,
+                                                        @RequestBody PostRequestDto postRequestDto,
+                                                        @AuthenticationPrincipal UserDetailsImpl userDetails){
+        try {
+            PostResponseDto responseDto = adminService.updatePost(postId,postRequestDto,userDetails.getUser());
+            return ResponseEntity.ok().body(responseDto);
+        }catch (IllegalArgumentException | RejectedExecutionException e){
+            return ResponseEntity.badRequest().body(new CommonResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+        }
+    }
 
 
     //게시글 삭제
--- src/main/java/com/sparta/iforest/admin/AdminService.java
+++ src/main/java/com/sparta/iforest/admin/AdminService.java
@@ -19,17 +19,24 @@ public class AdminService {
     private final UserRepository userRepository;
 
 
-//
-//    //관리자 게시글 수정
-//    @Transactional
-//    public PostResponseDto updatePost(Long postId, PostRequestDto dto, String user) {
-//        //포스트 가져오기
-//        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
-//        post.adminUpdatePost(dto, user);
-//        postRepository.save(post);
-//        return new PostResponseDto(post);
-//
-//    }
+
+    //관리자 게시글 수정
+    @Transactional
+    public PostResponseDto updatePost(Long postId, PostRequestDto dto, User user) {
+        //포스트 가져오기
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
+        UserRoleEnum userRole = user.getRole();
+
+        if (userRole == UserRoleEnum.ADMIN){
+            //수정
+            post.adminUpdatePost(dto, user.getUsername());
+            postRepository.save(post);
+            return new PostResponseDto(post);
+        } else {
+            throw new IllegalArgumentException ("관리자만 수정 할 수 있습니다. ");
+        }
+
+    }
 
 
 
